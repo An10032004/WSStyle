346c377cf5ba46a9c82adc200133456e20f5bb1d
duong an <duongansbsb@gmail.com>
2026-03-09 08:31:42 +0700

    fix backend project files

---
+++ Fashion-B2BWL-/src/main/java/com/fashionstore/core/FashionB2BwlBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fashionstore.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashionB2BwlBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FashionB2BwlBackendApplication.class, args);
+	}
+
+}
---
+++ Fashion-B2BWL-/src/test/java/com/fashionstore/core/FashionB2BwlBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fashionstore.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FashionB2BwlBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
